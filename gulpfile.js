d486149c351308b33615badf4f58b6d4a050b65b
leovoon <voonlihhaur@gmail.com>
2020-09-15 21:57:40 +0800

    minor edit

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,6 @@ const cssnano = require('cssnano');
 const imagemin = require('gulp-imagemin');
 const connect = require('gulp-connect'); // Runs a local webserver
 const open = require('gulp-open'); // Opens a URL in a web browser
-const exec = require('child_process').exec; // run command-line programs from gulp
-const execSync = require('child_process').execSync; // command-line reports
 var replace = require('gulp-replace');
 
 
@@ -98,28 +96,10 @@ function server(done) {
     done();
 }
 
-// Commit and push files to Git
-function git(done) {
-    return exec('git add . && git commit -m "netlify deploy" && git push');
-    done();
-}
 
-// Watch for netlify deployment
-function netlify(done) {
-    return new Promise(function(resolve, reject) {
-        console.log(execSync('netlify watch').toString());
-        resolve();
-    });
-}
-
-// Preview Deployment
-function netlifyOpen(done) {
-    return exec('netlify open:site');
-    done();
-}
 
 // Deploy command
-exports.deploy = series(git, netlify, netlifyOpen);
+exports.deploy = series( parallel(scssTask, jsTask, imgMinifiedTask));
 
 
 
