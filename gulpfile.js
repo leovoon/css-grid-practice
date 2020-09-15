e959e9528b568192182ff5144d030bcf3bef389e
leovoon <voonlihhaur@gmail.com>
2020-09-15 21:34:52 +0800

    netlify deploy

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,10 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const imagemin = require('gulp-imagemin');
+const connect = require('gulp-connect'); // Runs a local webserver
+const open = require('gulp-open'); // Opens a URL in a web browser
+const exec = require('child_process').exec; // run command-line programs from gulp
+const execSync = require('child_process').execSync; // command-line reports
 var replace = require('gulp-replace');
 
 
@@ -72,11 +76,57 @@ function watchTask(){
     );    
 }
 
-// Export the default Gulp task so it can be run
-// Runs the scss and js tasks simultaneously
-// then runs cacheBust, then watch task
+// Launch Chrome web browser
+// https://www.npmjs.com/package/gulp-open
+function openBrowser(done) {
+    var options = {
+    uri: 'http://localhost:8080'
+    };
+    return src('./')
+    .pipe(open(options));
+    done();
+}
+
+// Gulp plugin to run a webserver (with LiveReload)
+// https://www.npmjs.com/package/gulp-connect
+function server(done) {
+    return connect.server({
+    root: './',
+    port: 8080,
+    debug: true,
+    });
+    done();
+}
+
+// Commit and push files to Git
+function git(done) {
+    return exec('git add . && git commit -m "netlify deploy" && git push');
+    done();
+}
+
+// Watch for netlify deployment
+function netlify(done) {
+    return new Promise(function(resolve, reject) {
+        console.log(execSync('netlify watch').toString());
+        resolve();
+    });
+}
+
+// Preview Deployment
+function netlifyOpen(done) {
+    return exec('netlify open:site');
+    done();
+}
+
+// Deploy command
+exports.deploy = series(git, netlify, netlifyOpen);
+
+
+
 exports.default = series(
     parallel(scssTask, jsTask, imgMinifiedTask), 
+    openBrowser, 
+    server,
     cacheBustTask,
     watchTask
 );
