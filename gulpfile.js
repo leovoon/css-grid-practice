b774aab4d405135029ddac015269235b3d7e7dac
leovoon <voonlihhaur@gmail.com>
2020-09-16 12:28:15 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,6 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const imagemin = require('gulp-imagemin');
-const connect = require('gulp-connect'); // Runs a local webserver
-const open = require('gulp-open'); // Opens a URL in a web browser
 var replace = require('gulp-replace');
 
 
@@ -74,27 +72,7 @@ function watchTasks(){
     );    
 }
 
-// Launch Chrome web browser
-// https://www.npmjs.com/package/gulp-open
-function openBrowser(done) {
-    var options = {
-    uri: 'http://localhost:8080'
-    };
-    return src('./')
-    .pipe(open(options));
-    done();
-}
 
-// Gulp plugin to run a webserver (with LiveReload)
-// https://www.npmjs.com/package/gulp-connect
-function server(done) {
-    return connect.server({
-    root: './',
-    port: 8080,
-    debug: true,
-    });
-    done();
-}
 
 
 
@@ -105,7 +83,6 @@ exports.deploy = series( parallel(scssTask, jsTask, imgMinifiedTask));
 
 exports.default = series(
     parallel(scssTask, jsTask, imgMinifiedTask), 
-    openBrowser, 
     server,
     cacheBustTask,
     watchTasks
