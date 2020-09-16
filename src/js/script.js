d0b04161bfbd9752acc31d1e409b1a2032bb914e
leovoon <voonlihhaur@gmail.com>
2020-09-16 16:02:17 +0800

    Update html css js

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,12 @@
 import "../scss/style.scss" 
 console.log('script loaded')
+
+const shuffleBtn = document.getElementById('btnShuffle')
+const shuffleItems = [...document.querySelectorAll('.item')]
+
+    shuffleBtn.addEventListener('click', () => {
+        shuffleItems.forEach((item) => {
+            const random = Math.floor(Math.random() * 4)
+        item.setAttribute('style', `order: ${random};`)
+        })
+    })
